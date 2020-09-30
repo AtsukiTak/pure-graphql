5b091cfc6ef8d5a9de9d1a42776b68c78bbb2615
AtsukiTak <takatomgoo@gmail.com>
2020-09-30 18:57:45 +0900

    fix test code

--- pure-graphql-client-codegen/tests/plugin.test.js
+++ pure-graphql-client-codegen/tests/plugin.test.js
@@ -1,13 +1,13 @@
 const graphql = require("graphql");
 const { plugin } = require("../dist/index");
 
-it("should emits Decoder definition", () => {
+it("should emits Decoder definition", async () => {
   const schema = graphql.buildSchema(`
     type Query {
       id: String!
     }
   `);
-  const result = plugin(schema, [], null);
+  const result = await plugin(schema, [], null);
 
   const expected = expect.stringContaining("export const Decoder: D.Decoder");
 
